0c5c691288ded3ad76dbabe8eb7f7b52e943b3d0
Emmanuel Erim <bryonerim@gmail.com>
2022-12-23 12:55:31 +0100

    mobile done

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color:'rgb(83,83,170)'}}>Microskool App</Text>
+      <Button  title='Login' color={'rgb(83,83,170)'} onPress={()=>{
+        alert("Yes We Cans")
+      }}/>
+      <View >
+<TextInput/>
+        </View>
       <StatusBar style="auto" />
     </View> 
   );
